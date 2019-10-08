556292f2efa1df1ec0a3694b4d011307cc290fe1
Bipul Senapati <bsenapati@xpanxion.com>
2019-10-08 10:34:25 +0530

    orderservice

--- BLL/Order/OrderService.cs
+++ BLL/Order/OrderService.cs
@@ -25,9 +25,11 @@ namespace FactoryShopping.Domain.Order
             try
             {
                 var addOrder = _ocontext.cart.Where(o => o.UserId == order.UserId).FirstOrDefault();
+                var product = _ocontext.Products.Where(p => p.PId == order.PId).FirstOrDefault();
                 order.Price = addOrder.Price;
                 order.OrderedQuantity = addOrder.OrderQuantity;
                 order.PId = addOrder.PId;
+                var productName = product.Name;
                 _ocontext.SaveChanges();
                 return true;
             }
